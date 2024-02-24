ebcf8bb3f815b31b8dbb859e7229a7401316cde9
Christian Alexander <christian.alexanderato@gmail.com>
2024-02-24 08:19:51 -0800

    backend complete

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const PORT = process.env.PORT;
+const BooksRouter = require("./controllers/Books");
+const AboutRouter = require("./controllers/about");
 
 // APP OBJECT
 const app = express();
@@ -11,12 +13,15 @@ const app = express();
 // MIDDLEWARE
 app.use(morgan("dev"));
 app.use(cors());
+app.use(express.json());
 
-// ROUTER
+// ROUTERS
+app.use("/books", BooksRouter);
+app.use("/about", AboutRouter);
 
 // TEST ROUTE
 app.get("/", (req, res) => {
-  console.log("Hello Bookworm");
+  res.send("Hello Bookworm");
 });
 
 // LISTENER
